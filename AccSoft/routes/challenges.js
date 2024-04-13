a1cb98431e7ae807741afec65740d247716911bf
Albertojose-um-es <albertojose.hernandezg@um.es>
2024-04-13 18:59:27 +0200

    Challenges y eleccion de ellos

--- AccSoft/routes/challenges.js
+++ AccSoft/routes/challenges.js
@@ -6,7 +6,7 @@ var app = express()
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('/play', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('play', {tittle: 'Jugar'})
     });
 
@@ -18,6 +18,11 @@ router.get('/info', function(req, res, next) {
 router.get('/contrastChallenge', function(req, res, next) {   
     res.render('contrastChallenge', {tittle: 'Desafío de contraste'})
     });
+
+router.get('/contrastChallenge2', function(req, res, next) {   
+    res.render('contrastChallenge2', {tittle: 'Desafío de contraste'})
+    });
+    
 
 module.exports = router;
 // Compare this snippet from AccSoft/views/index.hbs:
